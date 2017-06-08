6c5579ba0ddad91ae5968127e9244d42ec0e07cf
pengdaan <249049410@qq.com>
2017-06-08 19:02:48 +0800

    提交report路径修改

--- XSX_InTerFace/test_run/run_pmstest.py
+++ XSX_InTerFace/test_run/run_pmstest.py
@@ -5,14 +5,14 @@ import sys
 
 sys.path.append('..')#.. 代表当前路径的上一级路径
 import unittest
-from XSX_InTerFace.Setting.HTMLTestRunner import HTMLTestRunner
+from XSX_InTerFace.Setting.HTMLTestRunnerCN import HTMLTestRunner
 test_dir='/xux_project/XSX_InTerFace/pms'
 test_filename='/xux_project/XSX_InTerFace/report/'
 discover=unittest.defaultTestLoader.discover(test_dir,pattern='test*.py')
 
 if __name__ =="__main__":
     now=time.strftime("%y-%m-%d %H_%M_%S", time.localtime())
-    filename ='/xux_project/report/'+now+'_result.html'
+    filename ='/xux_project/XSX_InTerFace/report/'+now+'_result.html'
     fp=open(filename,'wb')
     runner =HTMLTestRunner(stream=fp,
                            title='pms_Interface Test Test_Report',
